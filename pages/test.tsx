1e64088a6ed67c581134a0adad26e38c927986d4
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-03-31 11:50:48 -0300

    Página Dashboard importada para testes.

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,10 +1,12 @@
 import "../styles/pageDefaultStyles.css";
 import LoginForm from "../components/loginForm";
+import Dashboard from "../components/Dashboard";
 
 export default function Home() {
   return (
-    <main className="absolute top-0 left-0 overflow-x-hidden w-full h-full bg-gray-500">
-      <LoginForm/>
+    <main className="absolute top-0 left-0 overflow-x-hidden w-full h-full bg-gray-800">
+      {/*<LoginForm/>*/}
+      {<Dashboard/>}
     </main>
   );
 }
