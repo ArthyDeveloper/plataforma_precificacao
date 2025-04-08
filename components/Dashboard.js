0d78e11502880c0a49c309896d42b7e0ea6eb73c
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-04-08 17:30:34 -0300

    Renomeado para criar páginas distintas de Usuário e Admin.

--- components/Dashboard.js
+++
@@ -1,9 +0,0 @@
-const Dashboard = () => {
-    return(
-        <div className="absolute top-0 left-0 overflow-x-hidden w-full h-full bg-gray-500">
-            <h1>Test</h1>
-        </div>
-    );
-};
-
-export default Dashboard;
