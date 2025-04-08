035523516cf760dd44696ace0d7316df00487406
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-04-08 17:31:28 -0300

    Dashboard de Usuário protótipo.

---
+++ components/Dashboard_Client.js
@@ -0,0 +1,9 @@
+const Dashboard_Client = () => {
+    return(
+        <div className="absolute top-0 left-0 overflow-x-hidden w-full h-full bg-gray-500 overflow-x-hidden">
+            <h1>Test</h1>
+        </div>
+    );
+};
+
+export default Dashboard_Client;
