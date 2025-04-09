5ab5bd1427bcbae4e5a7c2671249b4f5c5fb8329
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-04-09 17:53:04 -0300

    Criando layout de Registro e Update de documentos do Dashboard de Admin.

--- components/Dashboard_Admin.js
+++ components/Dashboard_Admin.js
@@ -1,22 +1,95 @@
-import styles from "../styles/dashboardAdmin.css";
+import React, {useState} from "react";
+import "../styles/dashboardAdmin.css";
 
 const Dashboard_Admin = () => {
+  const [user, setUser] = useState("");
+  const [senha, setSenha] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [admin, setAdmin] = useState("");
+  const [adminPwd, setAdminPwd] = useState("");
+  const [activePage, setActivePage] = useState("");
+
+  const mudarPágina = (página) => {
+    setActivePage(página);
+  }
+
+  const buttonClasses = (página) => activePage === página ? "!bg-gray-900 text-white" : "!bg-gray-800";
+
+  async function register(){
+    console.log(admin, adminPwd, user, senha, email, telefone);
+
+    if(user == "" || senha == "" || email == "" || telefone == ""){
+      alert("Há campos vazios!")
+    } else {
+      const url = `/api/register/${admin}/${adminPwd}/${user}/${senha}/${email}/${telefone}`;
+      fetch(url,{
+        method: "POST"
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
+  }
+
+  async function update(){
+    alert("Update!");
+  }
+
   return(
     <div className="absolute top-0 left-0 w-full h-full bg-gray-500 overflow-x-hidden">
-      <div className="relative rounded-sm top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[500px] h-[400px] bg-gray-700 overflow-hidden">
+      <div className="relative rounded-sm top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[400px] h-[500px] bg-gray-700 overflow-hidden">
         {/* Top Bar - Options */}
         <div className="relative rounded-t-sm w-full h-[40px] bg-gray-800 justify-evenly flex text-white text-center">
-          <button className="btnAdminSelector">Users</button>
-          <button className="btnAdminSelector">Register</button>
-          <button className="btnAdminSelector">Remove</button>
+          <button className={`btnAdminSelector ${buttonClasses("Usuários")}`}
+            onClick={() => mudarPágina("Usuários")}
+          >Usuários</button>
+          <button className={`btnAdminSelector ${buttonClasses("Registrar")}`}
+            onClick={() => mudarPágina("Registrar")}
+          >Registrar</button>
+          <button className={`btnAdminSelector ${buttonClasses("Update")}`}
+            onClick={() => mudarPágina("Update")}
+          >Update</button>
         </div>
 
-        {/* Register Form */}
-        <div className="relative w-full h-auto bg-gray-700">
-          <input placeholder="Nome"></input>
-          <input placeholder="Email"></input>
-          <input placeholder="Senha"></input>
-          <input placeholder="Telefone"></input>
+        <div className="relative w-full h-[90%] bg-gray-700">
+          {/* Exibe páginas de acordo com estado ativo. */}
+
+          {/* Users Form */}
+          {activePage === "Usuários" && (
+            <h1>Usuários</h1>
+          )}
+
+          {/* Register Form */}
+          {activePage === "Registrar" && (
+            <div className="adminInputDiv">
+              <input value={user} onChange={(e) => setUser(e.target.value)} className="adminInput !mt-0" placeholder="Nome"/>
+              <input value={senha} onChange={(e) => setSenha(e.target.value)} className="adminInput" placeholder="Senha"/>
+              <input value={email} onChange={(e) => setEmail(e.target.value)} className="adminInput" placeholder="Email"/>
+              <input value={telefone} onChange={(e) => setTelefone(e.target.value)} className="adminInput" placeholder="Telefone"/>
+              <input value={admin} onChange={(e) => setAdmin(e.target.value)} className="adminInput" placeholder="Admin"/>
+              <input value={adminPwd} type="password" onChange={(e) => setAdminPwd(e.target.value)} className="adminInput" placeholder="AdminPwd"/>
+              <button onClick={register} className="adminRegisterBtn">Registrar</button>
+            </div>
+          )}
+
+          {/* Update Form */}
+          {activePage === "Update" && (
+            <div className="adminInputDiv">
+              <input value={user} onChange={(e) => setUser(e.target.value)} className="adminInput !mt-0" placeholder="Nome"/>
+              <input value={senha} onChange={(e) => setSenha(e.target.value)} className="adminInput" placeholder="Senha"/>
+              <input value={email} onChange={(e) => setEmail(e.target.value)} className="adminInput" placeholder="Email"/>
+              <input value={telefone} onChange={(e) => setTelefone(e.target.value)} className="adminInput" placeholder="Telefone"/>
+              <input value={admin} onChange={(e) => setAdmin(e.target.value)} className="adminInput" placeholder="Admin"/>
+              <input value={adminPwd} type="password" onChange={(e) => setAdminPwd(e.target.value)} className="adminInput" placeholder="AdminPwd"/>
+              <button onClick={update} className="adminRegisterBtn">Update</button>
+            </div>
+          )}
+
         </div>
       </div>
     </div>
