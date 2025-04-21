5311c664a786247ff56e2b5952a9f12d2f2e13e0
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-04-20 23:01:10 -0300

    Botões agora alternam menus.

--- components/Dashboard_Client.js
+++ components/Dashboard_Client.js
@@ -8,6 +8,15 @@ const Dashboard_Client = ({user}) => {
   const mesAtual = meses[new Date().getMonth()];
   const [ano, setAno] = useState(anoAtual);
   const [mes, setMes] = useState(mesAtual);
+
+  // Mudança de painéis;
+  const [activePage, setActivePage] = useState("Dashboard");
+  
+  const mudarPágina = (página) => {
+    setActivePage(página);
+  }
+  
+  const buttonClasses = (página) => activePage === página ? "!border-neutral-500" : "border-neutral-700";
   
   // Define lista de Anos a partir do ano inicial;
   const years = [];
@@ -54,73 +63,82 @@ const Dashboard_Client = ({user}) => {
 
   return(
     <div className="absolute top-0 left-0 w-full h-full bg-neutral-950 overflow-x-hidden">
-      <div className="dashboardContentDiv centerDiv">
+      <div className="clientContentDiv centerDiv">
 
-        {/* Dashboard */}
-        <div className="dashboardContainer centerDiv">
+        {/* Validade */}
+        {activePage === "Validade" && (
+          <div className="dashboardContainer centerDiv">
 
-          {/* Seletores de painéis. */}
-          <div className="seletoresPaineisDiv hoverEffect">
-            <button className="seletorPaineisBtn hoverEffect">Validade</button>
-            <button className="seletorPaineisBtn hoverEffect">Dashboard</button>
-            <button className="seletorPaineisBtn hoverEffect">Contato</button>
+            <div className="seletoresPaineisDiv hoverEffect">
+              <button onClick={() => mudarPágina("Validade")} className={`seletorPaineisBtn hoverEffect ${buttonClasses("Validade")}`}>Validade</button>
+              <button onClick={() => mudarPágina("Dashboard")} className={`seletorPaineisBtn hoverEffect ${buttonClasses("Dashboard")}`}>Dashboard</button>
+              <button onClick={() => mudarPágina("Contato")} className={`seletorPaineisBtn hoverEffect ${buttonClasses("Contato")}`}>Contato</button>
+            </div>
+            
           </div>
+        )}
 
-          {/* Div Arquivos e Gráfico */}
-          <div className="containerArquivosGrafico">
+        {/* Dashbaord */}
+        {activePage === "Dashboard" && (
+          <div className="dashboardContainer centerDiv">
 
-            {/* Painel de arquivos. */}
-            <div className="filesDiv hoverEffect">
+            <div className="seletoresPaineisDiv hoverEffect">
+              <button onClick={() => mudarPágina("Validade")} className={`seletorPaineisBtn hoverEffect ${buttonClasses("Validade")}`}>Validade</button>
+              <button onClick={() => mudarPágina("Dashboard")} className={`seletorPaineisBtn hoverEffect ${buttonClasses("Dashboard")}`}>Dashboard</button>
+              <button onClick={() => mudarPágina("Contato")} className={`seletorPaineisBtn hoverEffect ${buttonClasses("Contato")}`}>Contato</button>
+            </div>
 
-              {/* Header e opções. */}
-              <div className="filesHeaderDiv">
-                <h1 className="filesH1">Relatórios</h1>
-                <div className="selectorsDiv">
-                  <select className="selectorInput" name="ano" value={ano} onChange={termosPesquisa}>
-                    {years.map((year) => (
-                      <option key={year} value={year}>
-                        {year}
-                      </option>
-                    ))}
-                  </select>
-                  <select className="selectorInput ml-1 !w-[80px]" name="mês" value={mes} onChange={termosPesquisa}>
-                    <option value="janeiro">Janeiro</option>
-                    <option value="fevereiro">Fevereiro</option>
-                    <option value="março">Março</option>
-                    <option value="abril">Abril</option>
-                    <option value="maio">Maio</option>
-                    <option value="junho">Junho</option>
-                    <option value="julho">Julho</option>
-                    <option value="agosto">Agosto</option>
-                    <option value="setembro">Setembro</option>
-                    <option value="outubro">Outubro</option>
-                    <option value="novembro">Novembro</option>
-                    <option value="dezembro">Dezembro</option>
-                  </select>
+            <div className="containerArquivosGrafico">
+
+              <div className="filesDiv hoverEffect">
+
+                <div className="filesHeaderDiv">
+                  <h1 className="filesH1">Relatórios</h1>
+                  <div className="selectorsDiv">
+                    <select className="selectorInput" name="ano" value={ano} onChange={termosPesquisa}>
+                      {years.map((year) => (
+                        <option key={year} value={year}>
+                          {year}
+                        </option>
+                      ))}
+                    </select>
+                    <select className="selectorInput ml-1 !w-[80px]" name="mês" value={mes} onChange={termosPesquisa}>
+                      <option value="janeiro">Janeiro</option>
+                      <option value="fevereiro">Fevereiro</option>
+                      <option value="março">Março</option>
+                      <option value="abril">Abril</option>
+                      <option value="maio">Maio</option>
+                      <option value="junho">Junho</option>
+                      <option value="julho">Julho</option>
+                      <option value="agosto">Agosto</option>
+                      <option value="setembro">Setembro</option>
+                      <option value="outubro">Outubro</option>
+                      <option value="novembro">Novembro</option>
+                      <option value="dezembro">Dezembro</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className="filesBox">
+                  <div className="file">
+                    <h1 className="fileName">01.01 - 14.01</h1>
+                    <button className="downloadBtn" onClick={() => updateDataset()}>
+                      <svg className="downloadSvg centerDiv size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                      </svg>
+                    </button>
+                  </div>
                 </div>
               </div>
 
-              {/* Arquivos. */}
-              <div className="filesBox">
-                <div className="file">
-                  <h1 className="fileName">01.01 - 14.01</h1>
-                  <button className="downloadBtn" onClick={() => updateDataset()}>
-                    <svg className="downloadSvg centerDiv size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                    </svg>
-                  </button>
+              <div className="graficoDiv hoverEffect">
+                <div className="graficoDiv2">
+                  <Grafico datasetGrafico={datasetGrafico} className="grafico" />
                 </div>
               </div>
             </div>
-
-            {/* Gráfico */}
-            <div className="graficoDiv hoverEffect">
-              <div className="graficoDiv2">
-                <Grafico datasetGrafico={datasetGrafico} className="grafico" />
-              </div>
-            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
