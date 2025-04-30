d8f5dbfd9bbff430b0f0001f10418322a652bcb1
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-04-30 17:47:19 -0300

    Botão de download não aparece quando arquivo não tem link de download.  Nome do usuário é mostrado na saudação do dashboard, agora centralizado também.

--- components/Dashboard_Client.js
+++ components/Dashboard_Client.js
@@ -132,7 +132,9 @@ const Dashboard_Client = ({user}) => {
             {<PagesButtons mudarPágina={mudarPágina} buttonClasses={buttonClasses} />}
             <div className="informationsContainer">
               <div className="infoDiv1 hoverEffect">
-                <h1 className="titleInfo">Olá, Usuário!</h1>
+                <h1 className="titleInfo userGreeting">
+                  Olá, {userData ? userData.searchUser?.user?.nameGreeting+"!" || "..." : "..."}
+                </h1>
               </div>
               <div className="infoDiv2 hoverEffect">
                 <div className="infoStatusDiv infoSubDiv">
@@ -178,11 +180,11 @@ const Dashboard_Client = ({user}) => {
                 <div className="filesBox">
                   {[1, 2, 3, 4, 5].map((key) => {
                     const file = userData?.searchUser?.user_DB?.resumes?.[ano]?.[mes]?.[key];
-                    if (file && file[0] === true) {
+                    if (file && file.scheduled === true) {
                       return (
                         <div key={key} className="file">
-                          <h1 className="fileName">{file[1]}</h1>
-                          <button className="downloadBtn" onClick={() => updateDataset()}>
+                          <h1 className="fileName">{file.fileName}</h1>
+                          <button className="downloadBtn" disabled={file.fileLink === "none"} onClick={() => updateDataset()}>
                             <svg className="downloadSvg centerDiv size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                               <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
                             </svg>
