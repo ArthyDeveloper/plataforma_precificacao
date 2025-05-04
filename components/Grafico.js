36ba1c95a4d65e23ebade08abc24d89138eaae27
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-05-04 20:55:50 -0300

    Pequenas estilizações no gráfico.

--- components/Grafico.js
+++ components/Grafico.js
@@ -8,26 +8,22 @@ const Grafico = ({datasetGrafico}) => {
         data={datasetGrafico}
         margin={{
           left: -15,
-          right: 5,
+          right: 30,
           bottom: 5,
-          top: 10
+          top: 20
         }}
       >
-        <CartesianGrid strokeDasharray="3 3" />
+        <CartesianGrid strokeDasharray="4 4" />
         <XAxis
           dataKey="name"
           interval={0}
         />
         <YAxis
-          domain={["dataMin - 100", "dataMax + 100"]}
-          tickCount={4}
+          domain={["dataMin", "dataMax"]}
+          tickCount={5}
         />
         <Tooltip />
         <Legend />
-        {/* Bar type Graph
-        <Line dataKey="Ganhando" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-        <Line dataKey="Perdendo" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
-        */}
         <Line type="bumpX" dataKey="Ganhando" fill="#8884d8" stroke="#00FA9A" strokeWidth="3" />
         <Line type="bumpX" dataKey="Perdendo" fill="#82ca9d" stroke="orange" strokeWidth="3" /> 
       </LineChart>
