16473966ca36d6162f63ba2e5036bdf4c79b5485
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-05-15 10:00:11 -0300

    Botão para carregar DB adicionado caso não carregue de primeira.
    Ajuste pixel perfect no grid.

--- components/Dashboard_Client.js
+++ components/Dashboard_Client.js
@@ -93,7 +93,7 @@ const Dashboard_Client = ({user}) => {
     const getUser = async () => {
       await gatherUserData();
     };
-    getUser();
+    getUser()
   }, []);
 
   useEffect(() => {
@@ -152,6 +152,16 @@ const Dashboard_Client = ({user}) => {
             {/*<PagesButtons mudarPágina={mudarPágina} buttonClasses={buttonClasses} />*/}
             <div className="informationsContainer">
               <div className="infoDiv1 hoverEffect">
+                {!userData ? (
+                  <>
+                    <button className="reloadDatabaseButton hoverEffect" onClick={async () => await gatherUserData()}>
+                      <svg className="size-6 stroke-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                      </svg>
+                    </button>
+                  </>
+                ) : null
+                }
                 <h1 className="titleInfo userGreeting">
                   Olá, {userData ? userData.searchUser?.user?.nameGreeting+"!" || "..." : "..."}
                 </h1>
