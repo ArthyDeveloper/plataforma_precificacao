5bf8de318cddaccc898472f6d29cf8a0b47bf321
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-05-20 17:42:39 -0300

    Telefone de contato agora é tirado do DB.

--- components/Dashboard_Client.js
+++ components/Dashboard_Client.js
@@ -71,6 +71,8 @@ const Dashboard_Client = ({user}) => {
     }
   }
 
+  const [telefoneContato, setContato] = useState("(00) 0000-0000")
+
   const gatherInfos = async () => {
     const url = "/api/client/getInfos";
     try {
@@ -92,7 +94,7 @@ const Dashboard_Client = ({user}) => {
       const data = await response.json();
 
       if (data) {
-        console.log("Dados obtidos:", data)
+        setContato(data.infos.user.number);
       } else {
         console.log("Erro")
       }
@@ -230,7 +232,9 @@ const Dashboard_Client = ({user}) => {
                     <svg className="phone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                       <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
                     </svg>
-                    <h2 className="infoDesc contatoDesc">(xx) xxxx-xxxx</h2>
+                    <h2 className="infoDesc contatoDesc">
+                      {telefoneContato ? telefoneContato || "(00) 0000-0000" : "(00) 0000-0000"}
+                    </h2>
                   </div>
                 </div>
               </div>
