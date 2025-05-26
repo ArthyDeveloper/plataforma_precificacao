da2eeae389a88735634f1bf4223ae0b9b7604c11
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-05-26 09:59:25 -0300

    Botão copia código de pagamento funcionando e estilizações.

--- components/Dashboard_Client.js
+++ components/Dashboard_Client.js
@@ -71,11 +71,12 @@ const Dashboard_Client = ({user}) => {
     }
   }
 
-  const [telefoneContato, setContato] = useState("(00) 0000-0000")
-  const [destinatario, setDestinatario] = useState("...")
-  const [banco, setBanco] = useState("...")
-  const [tipoPagamento, setTipoPagamento] = useState("...")
-  const [codigoPagamento, setCodigoPagamento] = useState("")
+  const [telefoneContato, setContato] = useState("(00) 0000-0000");
+  const [destinatario, setDestinatario] = useState("...");
+  const [banco, setBanco] = useState("...");
+  const [cpf, setCpf] = useState("...");
+  const [tipoPagamento, setTipoPagamento] = useState("...");
+  const [codigoPagamento, setCodigoPagamento] = useState("");
 
   const gatherInfos = async () => {
     const url = "/api/client/getInfos";
@@ -103,8 +104,9 @@ const Dashboard_Client = ({user}) => {
         setBanco(data.infos.pagamento.banco);
         setTipoPagamento(data.infos.pagamento.tipo);
         setCodigoPagamento(data.infos.pagamento.codigoPagamento);
+        setCpf(data.infos.pagamento.cpf);
       } else {
-        console.log("Erro")
+        console.log("Erro");
       }
 
     } catch (error) {
@@ -161,12 +163,21 @@ const Dashboard_Client = ({user}) => {
   }
 
   // Pop Up Renovar Serviço;
-  const [popUp, setPopUp] = useState(true); // DEBUG: Colocar false quando estiver terminado
+  const [popUp, setPopUp] = useState(false);
   const funcPopUp = () => {
     if(popUp){
       setPopUp(false);
     } else {
       setPopUp(true);
+    }
+  }
+
+  // Copiar Código de Pagamento;
+  const copiarCodigoPagamento = async () => {
+    try{
+      await navigator.clipboard.writeText(codigoPagamento);
+    } catch (error) {
+      console.error(`Erro ao copiar: ${error}`);
     }
   }
 
@@ -347,18 +358,28 @@ const Dashboard_Client = ({user}) => {
                   <div className="infoPagamentoDiv">
                     <h1 className="destinoH1">Informações</h1>
                     <h2>Destinatário: {destinatario || "..."}</h2>
+                    <h2>CPF: {cpf || "..."}</h2>
                     <h2>Banco: {banco || "..."}</h2>
                     <h2>Tipo: {tipoPagamento || "..."}</h2>
                   </div>
                 </div>
                 <div className="codigoPagamentoDiv">
                   <h1 className="codigoPagamentoH1">Código copia e cola</h1>
-                  <button className="copiarCodigoBtn">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
-                    </svg>
-                    Copiar
-                  </button>
+                  {!codigoPagamento ?(
+                    <button className="copiarCodigoBtn" disabled onClick={async () => copiarCodigoPagamento()}>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                      </svg>
+                      Copiar
+                    </button>
+                  ) : (
+                    <button className="copiarCodigoBtn" onClick={async () => copiarCodigoPagamento()}>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                      </svg>
+                      Copiar
+                    </button>
+                  )}
                   <h2 className="codigoPagamentoH2">{codigoPagamento}</h2>
                 </div>
               </div>
